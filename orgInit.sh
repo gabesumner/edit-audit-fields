e3cfe4e851f7c396ec82a3b6301c915ba600701d
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-05-15 11:52:11 -0700

    fix

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,9 @@
 # Create the demo org
 sfdx force:org:create -f config/project-scratch-def.json --setalias edit-audit-fields --setdefaultusername
 
+# Push the security settings first, otherwise the push will fail.
+sfdx force:source:deploy -m Settings:Security
+
 # Push the metadata into the new scratch org.
 sfdx force:source:push
 
